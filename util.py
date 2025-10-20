d46c523fc1e11a1eee1e5d1f30ed654bb05f04f6
AleksaMCode <aleksamcode@gmail.com>
2025-10-21 01:49:39 +0200

    Add time check

---
+++ util.py
@@ -0,0 +1,10 @@
+from datetime import datetime, time
+from zoneinfo import ZoneInfo
+
+
+def is_working_hours(tz="Europe/Paris"):
+    """
+    Returns True if the current time is between 7 AM and 6 PM.
+    """
+    now = datetime.now(ZoneInfo(tz)).time()
+    return time(7, 0) <= now <= time(18, 0)
