2a07eb5c57392978b38c5ab800c7aaad9a483d9e
AleksaMCode <aleksamcode@gmail.com>
2025-10-20 19:29:13 +0200

    Update status format

--- sniffer_status.py
+++ sniffer_status.py
@@ -3,7 +3,7 @@ import time
 
 from firebase_admin import db
 
-from settings import FIREBASE_NODE, FIREBASE_TIMEOUT_STATUS
+from settings import FIREBASE_NODE, FIREBASE_TIMEOUT_STATUS, TIMESTAMP_FORMAT
 
 
 def send_status():
@@ -11,8 +11,8 @@ def send_status():
         try:
             db.reference(f"/{FIREBASE_NODE}/status").update(
                 {
-                    "ssid": "working",
-                    "timestamp": datetime.datetime.now().strftime("%Y%m%d"),
+                    "status": "working",
+                    "timestamp": datetime.datetime.now().strftime(TIMESTAMP_FORMAT),
                 }
             )
             time.sleep(FIREBASE_TIMEOUT_STATUS)
