8f2e0a0a581d98f95930c5ae18f2be12c21d98d9
AleksaMCode <aleksamcode@gmail.com>
2025-10-21 10:56:52 +0200

    Add `TIMEZONE`

--- util.py
+++ util.py
@@ -1,8 +1,10 @@
 from datetime import datetime, time
 from zoneinfo import ZoneInfo
 
+from settings import TIMEZONE
 
-def is_working_hours(tz="Europe/Paris"):
+
+def is_working_hours(tz=TIMEZONE):
     """
     Returns True if the current time is between 7 AM and 6 PM.
     """
