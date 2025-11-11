3ffce647dbad16830e03efe2a8da4d136520d447
AleksaMCode <aleksamcode@gmail.com>
2025-11-11 17:54:10 +0100

    Update imports

--- collector/parser.py
+++ collector/parser.py
@@ -5,9 +5,9 @@ from zoneinfo import ZoneInfo
 
 from firebase_admin import db
 from scapy.layers.dot11 import Dot11ProbeReq
-
 from settings import FIREBASE_NODE, MAC_FILTER, RSA_KEY_PATH, TIMESTAMP_FORMAT, TIMEZONE
-from util import encrypt_data, is_working_hours, load_rsa_key_from_file
+
+from util.util import encrypt_data, is_working_hours, load_rsa_key_from_file
 
 RSA_KEY = load_rsa_key_from_file(RSA_KEY_PATH)
 
