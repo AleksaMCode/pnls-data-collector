2cdc32707a7df5ae3ea252d809525fb2274df6da
AleksaMCode <aleksamcode@gmail.com>
2025-12-13 10:22:35 +0100

    Fix Firebase init app issue

--- aggregator/aggregator_firebase_cleaner.py
+++ aggregator/aggregator_firebase_cleaner.py
@@ -1,4 +1,12 @@
-from aggregator.firebase import delete_all
+import firebase_admin
+from firebase_admin import credentials
+
+from .firebase import delete_all
+
+firebase_admin.initialize_app(
+    credentials.Certificate(FIREBASE_CREDENTIALS),
+    {"databaseURL": FIREBASE_DB_URL},
+)
 
 if __name__ == "__main__":
     delete_all()
