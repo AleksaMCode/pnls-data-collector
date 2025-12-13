40e34ea0fc8d01b6b753f68dd41878da23893adb
AleksaMCode <aleksamcode@gmail.com>
2025-12-13 10:25:24 +0100

    Add missing imports

--- aggregator/aggregator_firebase_cleaner.py
+++ aggregator/aggregator_firebase_cleaner.py
@@ -2,6 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 
 from .firebase import delete_all
+from .settings import FIREBASE_CREDENTIALS, FIREBASE_DB_URL
 
 firebase_admin.initialize_app(
     credentials.Certificate(FIREBASE_CREDENTIALS),
