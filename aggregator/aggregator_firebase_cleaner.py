c0e09a9f833448703e92184c9720d584ef47a798
AleksaMCode <aleksamcode@gmail.com>
2025-12-21 20:30:23 +0100

    Update cleaner to download data for backup first

--- aggregator/aggregator_firebase_cleaner.py
+++ aggregator/aggregator_firebase_cleaner.py
@@ -1,7 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials
 
-from .firebase import delete_all_by_nodes
+from aggregator.core.mongo.helpers import insert_from_firebase_to_mongo
+
+from .firebase import delete_all_by_nodes, download_all
 from .settings import FIREBASE_CREDENTIALS, FIREBASE_DB_URL
 
 firebase_admin.initialize_app(
@@ -10,4 +12,6 @@ firebase_admin.initialize_app(
 )
 
 if __name__ == "__main__":
+    data = download_all()
+    insert_from_firebase_to_mongo(data)
     delete_all_by_nodes()
