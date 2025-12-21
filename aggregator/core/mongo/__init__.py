d8ced07678006ecd7272930b09dd571315c85bbb
AleksaMCode <aleksamcode@gmail.com>
2025-12-21 20:27:10 +0100

    Implement `MongoDB`

---
+++ aggregator/core/mongo/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+import gridfs
+import pymongo
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+connection_url = (
+    f"mongodb://{os.getenv("MONGO_USERNAME")}"
+    f":{os.getenv("MONGO_PASSWORD")}@{os.getenv("MONGO_URL")}:{os.getenv("MONGO_PORT")}"
+    f"/{os.getenv("MONGO_DB")}?authSource={os.getenv("MONGO_AUTH_SOURCE")}"
+)
+
+
+client = pymongo.MongoClient(connection_url)
+
+db = client[os.getenv("MONGO_DB")]
+
+
+def get_collection(collection: str):
+    return db[collection]
+
+
+def get_bucket_for_write():
+    return gridfs.GridFSBucket(db)
+
+
+def get_bucket_for_read():
+    return gridfs.GridFS(db)
