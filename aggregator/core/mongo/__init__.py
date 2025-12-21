493199353c8c58d44aa778773859dc5b4c39a2c7
AleksaMCode <aleksamcode@gmail.com>
2025-12-21 20:54:49 +0100

    Add final fix for pipeline

--- aggregator/core/mongo/__init__.py
+++ aggregator/core/mongo/__init__.py
@@ -2,30 +2,4 @@ import os
 
 # Pipeline fix. See #45
 if os.getenv("ENV") != "test":
-    import gridfs
-    import pymongo
-    from dotenv import load_dotenv
-    load_dotenv()
-
-connection_url = (
-    f"mongodb://{os.getenv("MONGO_USERNAME")}"
-    f":{os.getenv("MONGO_PASSWORD")}@{os.getenv("MONGO_URL")}:{os.getenv("MONGO_PORT")}"
-    f"/{os.getenv("MONGO_DB")}?authSource={os.getenv("MONGO_AUTH_SOURCE")}"
-)
-
-
-client = pymongo.MongoClient(connection_url)
-
-db = client[os.getenv("MONGO_DB")]
-
-
-def get_collection(collection: str):
-    return db[collection]
-
-
-def get_bucket_for_write():
-    return gridfs.GridFSBucket(db)
-
-
-def get_bucket_for_read():
-    return gridfs.GridFS(db)
+    from ._init_runtime import get_collection, get_bucket_for_write, get_bucket_for_read
