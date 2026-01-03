5e7089b75c20ab93e9433355d8164aa8f9e99afc
AleksaMCode <aleksamcode@gmail.com>
2026-01-03 22:49:46 +0100

    Create `collector` status monitor

---
+++ collector_status_notifier/const.go
@@ -0,0 +1,18 @@
+package main
+
+const (
+	// Replace with your actual Mattermost webhook URL
+	FIREBASE_CREDENTIALS_FILE = "firebase_credentials.json"
+	FIREBASE_BASE_PATH        = "/"
+	TIMESTAMP_FORMAT          = "2006-01-02 15:04:05.999999"
+	// Sleep time between checking devices Firebase status
+	FIREBASE_TIMEOUT = 11
+)
+
+const (
+	DEVICE_RPI_1 = "RPI-1"
+	DEVICE_RPI_2 = "RPI-2"
+	DEVICE_RPI_3 = "RPI-3"
+)
+
+var Devices = []string{DEVICE_RPI_1, DEVICE_RPI_2, DEVICE_RPI_3}
