dd4dfd632f8d50e1fb493c8dd099b645e7f91be3
AleksaMCode <aleksamcode@gmail.com>
2026-01-06 15:46:06 +0100

    Fix false-positive bug Closes #84

--- collector_status_notifier/main.go
+++ collector_status_notifier/main.go
@@ -74,12 +74,25 @@ func main() {
 	checkDevicesStatus(client, ctx)
 
 	// Periodically check the Firebase database every `FIREBASE_TIMEOUT` minutes
-	ticker := time.NewTicker(FIREBASE_TIMEOUT * time.Minute)
+	ticker := time.NewTicker(FIREBASE_TIMEOUT * time.Second)
 	defer ticker.Stop()
 
 	for range ticker.C {
-		checkDevicesStatus(client, ctx)
+		if isWorkingHours() {
+			checkDevicesStatus(client, ctx)
+		} else {
+		}
 	}
+}
+
+func isWorkingHours() bool {
+	location, err := time.LoadLocation(TIMEZONE)
+	if err != nil {
+		log.Fatalf("Error loading timezone: %v", err)
+	}
+	now := time.Now().In(location)
+	hour := now.Hour()
+	return hour >= 7 && hour < 18
 }
 
 func getAbsoluteFirebasePath(credentialsFile string) (string, error) {
