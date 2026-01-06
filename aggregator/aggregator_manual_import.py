77876352e11e090ee0c49ee1d66cdc5ebc21ace0
AleksaMCode <aleksamcode@gmail.com>
2026-01-06 13:35:42 +0100

    Add date lambda for format checking

--- aggregator/aggregator_manual_import.py
+++ aggregator/aggregator_manual_import.py
@@ -7,7 +7,10 @@ from aggregator.settings import TIMESTAMP_FORMAT
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process data for a specific date")
     parser.add_argument(
-        "--date", required=True, help="Date to process in YYYY-MM-DD format"
+        "--date",
+        required=True,
+        type=lambda s: datetime.strptime(s, "%Y-%m-%d").date(),
+        help="Date to process in YYYY-MM-DD format",
     )
 
     args = parser.parse_args()
