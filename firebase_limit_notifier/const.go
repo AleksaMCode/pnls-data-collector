e98e8df3fb1aa814b28ddadc8636e9877a0bba4d
AleksaMCode <aleksamcode@gmail.com>
2026-01-07 12:38:47 +0100

    Implement `Firebase` limit microservice

---
+++ firebase_limit_notifier/const.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+	// Replace with your actual Mattermost webhook URL
+	FIREBASE_CREDENTIALS_FILE = "firebase_credentials.json"
+	FIREBASE_BASE_PATH        = "/"
+	LOG_FILE                  = "firebase_limit_notifier.log"
+)
