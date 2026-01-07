7f7e14ccec01cbfb856c7c95971141b17de642ac
AleksaMCode <aleksamcode@gmail.com>
2026-01-07 21:59:47 +0100

    Update const values

--- firebase_limit_notifier/const.go
+++ firebase_limit_notifier/const.go
@@ -5,4 +5,8 @@ const (
 	FIREBASE_CREDENTIALS_FILE = "firebase_credentials.json"
 	FIREBASE_BASE_PATH        = "/"
 	LOG_FILE                  = "firebase_limit_notifier.log"
+	R2_BUCKET_DIR             = "firebase-limit"
+	TIMEZONE                  = "Europe/Paris"
+	// Free tier has 1 GB limit on Realtime DB
+	FIREBASE_LIMIT_MB = 1_000
 )
