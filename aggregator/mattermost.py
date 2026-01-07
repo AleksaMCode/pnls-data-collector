1af7b2c9e991e50eca8a4f8afe48cf310edb09e0
AleksaMCode <aleksamcode@gmail.com>
2026-01-07 11:03:53 +0100

    Update `if-else` logic to sent complete msg to the Mattermost channel Closese #101

--- aggregator/mattermost.py
+++ aggregator/mattermost.py
@@ -5,13 +5,13 @@ from aggregator.util.util import send_webhook_message
 
 def publish_to_channel(data: dict, probe_req_count: int, import_date: date = None):
     # This is tightly coupled with stats data from firebase#publish_stats_data.
-    mattermost_msg = (
-        f"**Today's data has been aggregated.**\n"
-        if import_date is None
-        else f"**Data imported for date '{import_date}'.**\n"
-        f"(Total captured Probe Requests today: **{probe_req_count}**)"
-        f"* Total captured probe requests: {data['total_count']}\n"
-        f"* Total captured unique MAC addresses: {data['mac_count']}\n"
-        f"* Total captured unique SSIDs: {data['ssid_count']}\n"
-    )
+    mattermost_msg = f"""{
+        f"**Today's data has been aggregated.**" if not import_date
+        else f"**Data imported for date '{import_date}'.**"
+    }
+    (Total captured Probe Requests: **{probe_req_count}**)
+    * Total captured probe requests: {data['total_count']}
+    * Total captured unique MAC addresses: {data['mac_count']}
+    * Total captured unique SSIDs: {data['ssid_count']}
+    """
     send_webhook_message(mattermost_msg)
