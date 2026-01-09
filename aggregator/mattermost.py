204707201ec87752e6df3c5f8750de475fed2799
AleksaMCode <aleksamcode@gmail.com>
2026-01-09 23:27:42 +0100

    Fix Mattermost message format

--- aggregator/mattermost.py
+++ aggregator/mattermost.py
@@ -5,13 +5,17 @@ from aggregator.util.util import send_webhook_message
 
 def publish_to_channel(data: dict, probe_req_count: int, import_date: date = None):
     # This is tightly coupled with stats data from firebase#publish_stats_data.
-    mattermost_msg = f"""{
-        f"**Today's data has been aggregated.**" if not import_date
-        else f"**Data imported for date '{import_date}'.**"
-    }
-    (Total captured Probe Requests: **{probe_req_count}**)
-    * Total captured probe requests: {data['total_count']}
-    * Total captured unique MAC addresses: {data['mac_count']}
-    * Total captured unique SSIDs: {data['ssid_count']}
-    """
+    first_line = (
+        "**Today's data has been aggregated.**\n"
+        if not import_date
+        else f"**Data imported for date '{import_date}'.**\n"
+    )
+
+    mattermost_msg = (
+        first_line
+        + f"(Captured Probe Requests: **{probe_req_count}**)\n"
+        + f"* Total captured probe requests: {data['total_count']}\n"
+        + f"* Total captured unique MAC addresses: {data['mac_count']}\n"
+        + f"* Total captured unique SSIDs: {data['ssid_count']}\n"
+    )
     send_webhook_message(mattermost_msg)
