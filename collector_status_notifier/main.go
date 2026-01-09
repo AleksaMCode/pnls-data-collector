d0fdde8cd3b0921c60f362f6cd0a4a6c0ad01f13
AleksaMCode <aleksamcode@gmail.com>
2026-01-09 23:54:18 +0100

    Refactor the service

--- collector_status_notifier/main.go
+++ collector_status_notifier/main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"path/filepath"
 	"strings"
 	"time"
 
-	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/db"
 	"github.com/joho/godotenv"
-	"google.golang.org/api/option"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
@@ -48,26 +42,8 @@ func main() {
 	loadEnvVariables()
 	initLogging()
 
-	absPath, err := getAbsoluteFirebasePath(FIREBASE_CREDENTIALS_FILE)
-	if err != nil {
-		log.Fatalf("Error getting absolute path for credentials file: %v", err)
-	}
-
 	ctx := context.Background()
-	opt := option.WithCredentialsFile(absPath)
-	conf := &firebase.Config{
-		DatabaseURL: FIREBASE_DATABASE_URL,
-	}
-	app, err := firebase.NewApp(ctx, conf, opt)
-	if err != nil {
-		log.Fatalf("Error initializing Firebase app: %v", err)
-	}
-
-	// Get a reference to the Realtime Database
-	client, err := app.Database(ctx)
-	if err != nil {
-		log.Fatalf("Error getting database client: %v", err)
-	}
+	client := getFirebaseClient(ctx)
 
 	// Check before sleep
 	checkDevicesStatus(client, ctx)
@@ -81,24 +57,6 @@ func main() {
 			checkDevicesStatus(client, ctx)
 		}
 	}
-}
-
-func isWorkingHours() bool {
-	location, err := time.LoadLocation(TIMEZONE)
-	if err != nil {
-		log.Fatalf("Error loading timezone: %v", err)
-	}
-	now := time.Now().In(location)
-	hour := now.Hour()
-	return hour >= 7 && hour < 18
-}
-
-func getAbsoluteFirebasePath(credentialsFile string) (string, error) {
-	absPath, err := filepath.Abs(credentialsFile)
-	if err != nil {
-		return "", fmt.Errorf("failed to get current directory: %v", err)
-	}
-	return absPath, nil
 }
 
 func checkDevicesStatus(client *db.Client, ctx context.Context) {
@@ -151,55 +109,5 @@ func validateDeviceData(client *db.Client, ctx context.Context) {
 		} else {
 			log.Printf("Device `%s` was recently updated at %s", device, timestamp)
 		}
-	}
-}
-
-func validateFirebaseNode(deviceKey, today string) bool {
-	if deviceKey == "" || today == "" {
-		return false
-	}
-
-	// Check if the device key ends with today's date in the format YYYY-MM-DD
-	if len(deviceKey) > len(today) && deviceKey[len(deviceKey)-len(today):] == today {
-		// Check if the deviceKey contains any of the device names
-		for _, device := range Devices {
-			if strings.Contains(deviceKey, device) {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func sendMattermostMessage(message string) {
-	payload := map[string]any{
-		"text":     message,
-		"username": SERVICE_NAME,
-		// Use Gopher as a bot icon
-		"icon_url": "https://raw.githubusercontent.com/golang-samples/gopher-vector/refs/heads/master/gopher.svg",
-	}
-
-	payloadBytes, err := json.Marshal(payload)
-	if err != nil {
-		log.Fatalf("Failed to marshal message: %v", err)
-	}
-
-	req, err := http.NewRequest("POST", MATTERMOST_WEBHOOK_URL, bytes.NewBuffer(payloadBytes))
-	if err != nil {
-		log.Fatalf("Failed to create HTTP request: %v", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Failed to send message to Mattermost: %v", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		log.Printf("Mattermost webhook response: %s", resp.Status)
-	} else {
-		log.Println("Successfully sent message to Mattermost!")
 	}
 }
