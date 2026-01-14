2a978748dcf89b7417b6bf6e365994b4631a6379
AleksaMCode <aleksamcode@gmail.com>
2026-01-14 12:51:37 +0100

    Remove unused code

--- monitor_dashboard/src/components/auth/login/index.jsx
+++ monitor_dashboard/src/components/auth/login/index.jsx
@@ -34,7 +34,6 @@ export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
   const handleClickShowPassword = () => setShowPassword(!showPassword);
-  const handleMouseDownPassword = (event) => event.preventDefault();
 
   const validateInputs = () => {
     let isValid = true;
