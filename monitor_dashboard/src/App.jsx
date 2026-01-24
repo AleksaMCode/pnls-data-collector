b30a0c752ae67a658fdc85604f3e228a3f9058d0
AleksaMCode <aleksamcode@gmail.com>
2026-01-24 18:41:00 +0100

    Update paths, imports, and add Toast notification

--- monitor_dashboard/src/App.jsx
+++ monitor_dashboard/src/App.jsx
@@ -1,11 +1,13 @@
 import Login from './components/auth/login';
 
-import Home from './components/home';
-
-import { useRoutes } from 'react-router-dom';
+import { Navigate, useRoutes } from 'react-router-dom';
 import { AuthProvider } from './context/authContext';
 import ProtectedRoute from './components/protectedRoute';
 import PublicRoute from './components/publicRoute';
+import Dashboard from './components/dashboard/Dashboard';
+import { ToastContainer } from 'react-toastify';
+import DeviceView from './components/dashboard/DeviceView';
+import MainGrid from './components/dashboard/MainGrid';
 
 function App() {
   const routesArray = [
@@ -26,17 +28,33 @@ function App() {
       ),
     },
     {
-      path: '/home',
+      path: '/',
       element: (
         <ProtectedRoute>
-          <Home />
+          <Dashboard />
         </ProtectedRoute>
       ),
+      children: [
+        {
+          index: true,
+          element: <Navigate to="home" replace />,
+        },
+        { path: 'home', element: <MainGrid /> },
+        { path: 'device/:deviceId', element: <DeviceView /> },
+      ],
     },
   ];
   let routesElement = useRoutes(routesArray);
   return (
     <AuthProvider>
+      {}
+      <ToastContainer
+        position="top-center"
+        autoClose={4000}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+      />
       <div className="w-full h-screen flex flex-col">{routesElement}</div>
     </AuthProvider>
   );
