1ff8011ee1401033e289492e8ccb585216069c02
AleksaMCode <aleksamcode@gmail.com>
2026-02-09 23:06:09 +0100

    Fix typo

--- aggregator/core/orm/event.py
+++ aggregator/core/orm/event.py
@@ -5,7 +5,7 @@ from aggregator.core.orm.helpers import resolve_oui
 from aggregator.core.orm.models import MAC
 
 
-# before_flush - https://docs.sqlalchemy.org/en/21/orm/events.html#sqlalchemy.orm.SessionEvents
+# before_commit - https://docs.sqlalchemy.org/en/21/orm/events.html#sqlalchemy.orm.SessionEvents
 @event.listens_for(SessionFactory, "before_commit")
 def mac_oui_resolver(session):
     for obj in session.new.union(session.dirty):
