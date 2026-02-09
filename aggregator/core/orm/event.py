66e215b0fee3908efe27bbee345ecbe159a9fd81
AleksaMCode <aleksamcode@gmail.com>
2026-02-09 20:43:33 +0100

    Add event listener that will trigger MAC to OUI mapping

---
+++ aggregator/core/orm/event.py
@@ -0,0 +1,13 @@
+from sqlalchemy import event
+
+from aggregator.core.orm._init_runtime import SessionFactory
+from aggregator.core.orm.helpers import resolve_oui
+from aggregator.core.orm.models import MAC
+
+
+# before_flush - https://docs.sqlalchemy.org/en/21/orm/events.html#sqlalchemy.orm.SessionEvents
+@event.listens_for(SessionFactory, "before_commit")
+def mac_oui_resolver(session):
+    for obj in session.new.union(session.dirty):
+        if isinstance(obj, MAC) and obj.mac:
+            resolve_oui(session, obj)
