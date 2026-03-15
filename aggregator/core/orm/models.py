f424529f9d13d3ea9761c90a699dce2f454034ed
AleksaMCode <aleksamcode@gmail.com>
2026-03-15 15:18:43 +0100

    Move Enum to global util package

--- aggregator/core/orm/models.py
+++ aggregator/core/orm/models.py
@@ -16,12 +16,6 @@ from sqlalchemy.orm import declarative_base, relationship, validates
 Base = declarative_base()
 
 
-class Device(Enum):
-    RPI_1 = "RPI-1"
-    RPI_2 = "RPI-2"
-    RPI_3 = "RPI-3"
-
-
 class IEEERegistry(Enum):
     MA_L = "MA-L"
     MA_M = "MA-M"
