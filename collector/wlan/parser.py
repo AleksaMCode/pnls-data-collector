68ef5caa8825994b235a5e8e136b17346152d137
AleksaMCode <aleksamcode@gmail.com>
2026-05-03 18:52:50 +0200

    Refactor parser

--- collector/wlan/parser.py
+++ collector/wlan/parser.py
@@ -1,22 +1,20 @@
-import json
-import os
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-from firebase_admin import db
 from scapy.layers.dot11 import Dot11ProbeReq
 
-from util.logger import get_logger
-from util.util import encrypt_data, is_working_hours, load_rsa_key_from_file
-
-from .settings import (
-    FIREBASE_NODE,
+import collector.wlan.helpers as wifi_helpers
+from collector.core.firebase.helpers import publish_captured_date
+from collector.settings import (
     MAC_FILTER,
     RSA_KEY_PATH,
     SSID_FILTER,
     TIMESTAMP_FORMAT,
     TIMEZONE,
 )
+from collector.util.util import publish_captured_data_locally
+from util.logger import get_logger
+from util.util import encrypt_data, is_working_hours, load_rsa_key_from_file
 
 logger = get_logger(__name__)
 
@@ -30,7 +28,8 @@ def parse_ip_packet(packet):
     # Only capture data between 7 AM and 6 PM.
     if not is_working_hours(TIMEZONE):
         return
-    # Filter only Probe Request.
+    # Filter only Probe Request. This is probably no longer needed due to utilization of filter in AsyncSniffer.
+    # This just to safe in case the filter fails.
     if packet.haslayer(Dot11ProbeReq):
         ssid = None
         try:
@@ -42,27 +41,21 @@ def parse_ip_packet(packet):
             data = {
                 "mac": encrypt_data(RSA_KEY, packet.addr2),
                 "ssid": "*" if not ssid else ssid,
+                "channel": wifi_helpers.WLAN_CHANNEL,
                 "timestamp": datetime.fromtimestamp(
                     float(packet.time), tz=ZoneInfo(TIMEZONE)
                 ).strftime(TIMESTAMP_FORMAT)[:-3],
             }
-            today = datetime.now().strftime(TIMESTAMP_FORMAT.split(" ")[0])
 
+            today = datetime.now().strftime(TIMESTAMP_FORMAT.split(" ")[0])
             # Send to Firebase DB
             try:
-                db.reference(f"/{FIREBASE_NODE}-{today}/data").push(data)
+                publish_captured_date(data, today)
             except Exception as e:
                 logger.error(f"Firebase update failed: {str(e)}")
 
             # Save locally for backup
             try:
-                with open(
-                    os.path.join(
-                        os.path.dirname(os.path.abspath(__file__)), f"{today}-data.json"
-                    ),
-                    "a",
-                    encoding="utf-8",
-                ) as f:
-                    f.write(json.dumps(data) + "\n")
+                publish_captured_data_locally(data, today)
             except Exception as e:
                 logger.error(f"Local JSON append failed: {str(e)}")
