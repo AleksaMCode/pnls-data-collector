4fe9ea562d7812d4799af4c1da0cce5049d5aa46
AleksaMCode <aleksamcode@gmail.com>
2026-07-03 08:06:13 +0200

    Fix pipeline

--- aggregator/core/redis/__init__.py
+++ aggregator/core/redis/__init__.py
@@ -1,6 +1,16 @@
 import os
 
-# Fix for pipeline. See #10
-# TODO See if this can be done in another way
-if os.getenv("ENV") != "test":
-    from ._init_runtime import client
+import redis
+from dotenv import load_dotenv
+
+load_dotenv()
+
+REDIS_DB = 1
+
+client = redis.Redis(
+    host=os.getenv("REDIS_URL"),
+    port=int(os.getenv("REDIS_PORT", "6379")),
+    password=os.getenv("REDIS_PASSWORD"),
+    db=REDIS_DB,
+    decode_responses=True,
+)
