59486d7a33fb318c2e963aae08e0eb26eb660323
AleksaMCode <aleksamcode@gmail.com>
2026-07-04 13:09:57 +0200

    Add Redis db constant

--- aggregator/core/redis/__init__.py
+++ aggregator/core/redis/__init__.py
@@ -3,14 +3,15 @@ import os
 import redis
 from dotenv import load_dotenv
 
+from aggregator.settings import REDIS_CACHE_DB
+
 load_dotenv()
 
-REDIS_DB = 1
 
 client = redis.Redis(
     host=os.getenv("REDIS_URL"),
     port=int(os.getenv("REDIS_PORT", "6379")),
     password=os.getenv("REDIS_PASSWORD"),
-    db=REDIS_DB,
+    db=REDIS_CACHE_DB,
     decode_responses=True,
 )
