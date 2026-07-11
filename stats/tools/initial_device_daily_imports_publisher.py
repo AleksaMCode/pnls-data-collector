e92d66fcfd7a65cc8f302d144e5f91fcbe108efa
AleksaMCode <aleksamcode@gmail.com>
2026-07-11 08:41:00 +0200

    Format using `black`

--- stats/tools/initial_device_daily_imports_publisher.py
+++ stats/tools/initial_device_daily_imports_publisher.py
@@ -6,7 +6,9 @@ logger = get_logger(__name__)
 
 
 def publish_all_device_daily_imports():
-    logger.info("Fetching device daily imports from local DB for initial Supabase publish.")
+    logger.info(
+        "Fetching device daily imports from local DB for initial Supabase publish."
+    )
     data = get_all_data_from_daily_captured_stats_per_device()
     logger.info(f"Fetched {len(data)} device daily import rows.")
 
